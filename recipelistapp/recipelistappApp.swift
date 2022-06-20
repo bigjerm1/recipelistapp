c20179872427d0241353f05e6264c3b831608496
daniel jermaine <danieljermaine2@gmail.com>
2022-06-20 20:24:40 +0100

    Initial Commit

---
+++ recipelistapp/recipelistappApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipelistappApp.swift
+//  recipelistapp
+//
+//  Created by Apple on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct recipelistappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
